62aca57a22720cc438cab8b1dc3f08c44cd79143
Deniz Gunay <andacg1@hotmail.com>
2024-02-25 13:52:20 -0800

    chore: fixed Jest/Parcel Babel conflict

--- jest.config.ts
+++ jest.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   // bail: 0,
 
   // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "C:\\Users\\andac\\AppData\\Local\\Temp\\jest",
+  // cacheDirectory: "",
 
   // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
@@ -90,7 +90,9 @@ const config: Config = {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1"
+  }
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
---
+++ test/parseTekkenNotation.test.ts
@@ -0,0 +1,74 @@
+import { parseTekkenNotation } from "@/src";
+
+describe('parseTekkenNotation', () => {
+    test('parses basic Tekken notation', () => {
+        const tree = parseTekkenNotation('d/f 1+2, SEN 2, 3, 4');
+        const instructions = tree.moves[0].instructions;
+        expect(instructions).toMatchInlineSnapshot(`
+[
+  {
+    "inputs": [
+      {
+        "notation": "d",
+      },
+      {
+        "notation": "f",
+      },
+    ],
+    "notation": "d/f",
+    "slug": "df",
+    "type": "MOVEMENT",
+  },
+  {
+    "inputs": [
+      {
+        "notation": "1",
+      },
+      {
+        "notation": "2",
+      },
+    ],
+    "notation": "1+2",
+    "slug": "12",
+    "type": "ACTION",
+  },
+  {
+    "inputs": [],
+    "notation": "SEN",
+    "slug": "SEN",
+    "type": "SPECIAL",
+  },
+  {
+    "inputs": [
+      {
+        "notation": "2",
+      },
+    ],
+    "notation": "2",
+    "slug": "2",
+    "type": "ACTION",
+  },
+  {
+    "inputs": [
+      {
+        "notation": "3",
+      },
+    ],
+    "notation": "3",
+    "slug": "3",
+    "type": "ACTION",
+  },
+  {
+    "inputs": [
+      {
+        "notation": "4",
+      },
+    ],
+    "notation": "4",
+    "slug": "4",
+    "type": "ACTION",
+  },
+]
+`)
+    });
+});
